9dfa59c0495fd30b0f7d1a742d4d4b7b38346423
ahampriyanshu <ahampriyanshu@gmail.com>
2024-01-15 01:03:40 +0530

    fix: isOpened

--- app/components/EmailHeader/EmailViewHeader.tsx
+++ app/components/EmailHeader/EmailViewHeader.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import styles from './email-header.module.scss';
 import {
   Alert,
--- app/components/EmailPage/EmailPage.tsx
+++ app/components/EmailPage/EmailPage.tsx
@@ -1,18 +1,28 @@
 'use client';
-import React, { use, useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { MailContainer } from '../EmailContent/MailContainer';
 import { EmailViewHeader } from '../EmailHeader/EmailViewHeader';
 import { useEmailActions } from '@/app/hooks/useEmailActions';
+import { AppContext } from '@/app/AppContext';
+import { useRouter } from 'next/navigation';
 
 export const EmailPage = ({ id }: { id: string }) => {
+  const router = useRouter();
   const { updateEmailArgs } = useEmailActions();
+  const { state } = useContext(AppContext);
 
   useEffect(() => {
-    updateEmailArgs(id, { isOpened: true });
-    // return () => {
-    //   updateEmailArgs(id, { isOpened: false });
-    // };
-  }, [id]);
+    const emailData = state.emails || [];
+    const emailDatum = emailData.find((email) => email?.id === id);
+
+    if (!emailDatum) {
+      router.back();
+    }
+
+    if (!emailDatum?.isOpened) {
+      updateEmailArgs(id, { isOpened: true });
+    }
+  }, [updateEmailArgs, id]);
 
   return (
     <div className='h-100 w-100'>
